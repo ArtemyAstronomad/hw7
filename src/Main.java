3652eaf3b86a4c195402f0b0f0699f2d092311a6
ArtemyAstronomad <op2607@yandex.ru>
2024-01-02 02:00:59 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+ppublic class HW7 {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+        task9();
+        task10();
+    }
+    private static void task1 {
+    }
+}
